ad7da42fe5a7cdd85a91cd3191ddc40f3bd743ee
Darshan Pawar <darshan.pawar23@gmail.com>
2026-04-19 22:12:40 +0530

    added AppointmentFormIC

---
+++ src/components/AppointmentFormIC.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AppointmentFormIC() {
+  return (
+    <div>
+      <h2>Appointment Form</h2>
+      <input placeholder="Name" />
+      <input placeholder="Phone Number" />
+    </div>
+  );
+}
+
+export default AppointmentFormIC;
